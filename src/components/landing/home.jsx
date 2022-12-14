9de4b0b6109853fb508045ee741a33bd98143500
sebastian-wsd <seroven@websolutionsdev.com>
2022-12-14 17:41:49 -0500

    add layout home

--- src/components/landing/home.jsx
+++ src/components/landing/home.jsx
@@ -1,7 +1,9 @@
 import './styles/home-style.css'
 import imageComputer from '../../assets/images/landing_home_initial_computer.png'
+import iconWhatsapp from '../../assets/images/whatsapp_icon.png'
 
 export const Home = () => {
+
   return <div className="home-bg">
     <div className="container-screen h-full">
       <div className='home-layout'>
@@ -13,14 +15,24 @@ export const Home = () => {
           <span className='home-title-font'>
             NEGOCIO
           </span>
-          <div className='my-3'>
+          <div className='mt-10'>
             <span className='home-subtitle-font'>
-              Solicitando tu
+              Solicitando tu propia
             </span>
             <br />
             <span className='home-subtitle-font'>
               Página web
             </span>
+          </div>
+          <div className='home-quotation-layout'>
+            <button className='home-quotation-button'>
+              Cotización
+            </button>
+          </div>
+          <div className='home-contact-layout'>
+            <button className='home-contact-button'>
+              Contáctanos
+            </button>
           </div>
         </div>
         <div className='home-image-layout'>
@@ -28,6 +40,9 @@ export const Home = () => {
         </div>
       </div>
 
+    </div>
+    <div className='home-icon-wss'>
+      <img src={iconWhatsapp} />
     </div>
   </div>
 }
