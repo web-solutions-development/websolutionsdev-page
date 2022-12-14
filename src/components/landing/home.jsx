659f2d4ea13cd00c851e0ae409a9511892c47ccf
sebastian-wsd <seroven@websolutionsdev.com>
2022-12-13 19:37:38 -0500

    safe zone

--- src/components/landing/home.jsx
+++ src/components/landing/home.jsx
@@ -1,4 +1,33 @@
+import './styles/home-style.css'
+import imageComputer from '../../assets/images/landing_home_initial_computer.png'
 
 export const Home = () => {
-  return <div>LandingHome</div>
+  return <div className="home-bg">
+    <div className="container-screen h-full">
+      <div className='home-layout'>
+        <div className='home-title-layout'>
+          <span className='home-title-font'>
+            IMPULSA TU
+          </span>
+          <br />
+          <span className='home-title-font'>
+            NEGOCIO
+          </span>
+          <div className='my-3'>
+            <span className='home-subtitle-font'>
+              Solicitando tu
+            </span>
+            <br />
+            <span className='home-subtitle-font'>
+              Página web
+            </span>
+          </div>
+        </div>
+        <div className='home-image-layout'>
+          <img src={imageComputer} />
+        </div>
+      </div>
+
+    </div>
+  </div>
 }
