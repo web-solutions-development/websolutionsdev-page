e20f3ea8b101bba0e1777dd7f4bb43d6b1036fba
sebastian-wsd <seroven@websolutionsdev.com>
2022-12-21 21:02:47 -0500

    remove bootstrap

--- src/components/landing/pure/ProductCard.jsx
+++ src/components/landing/pure/ProductCard.jsx
@@ -1,23 +1,26 @@
 import React from "react"
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/news.css'
-import {Card} from 'react-bootstrap'
+// import { Card } from 'react-bootstrap'
 
-const ProductCard = props =>{
-  let {imgSrc,price,fecha} = props.data ;
+const ProductCard = props => {
+  const { imgSrc, price, fecha } = props.data;
   return (
-    <Card className="p-0 overflow-hidden  rounded-t-3xl ">
-      <div className=" overflow-hidden  p-0 ">
-        <Card.Img className="img" variant= "top" src={imgSrc}/>
-      </div>
-      <Card.Body className="contenText text-center ">
-        <Card.Title className="text-left ">{fecha}</Card.Title>
-        <Card.Title className="display-8 text-left ">{price}</Card.Title>
+    <div>
+      a
+    </div>
+    // <Card className="p-0 overflow-hidden  rounded-t-3xl ">
+    //   <div className=" overflow-hidden  p-0 ">
+    //     <Card.Img className="img" variant="top" src={imgSrc} />
+    //   </div>
+    //   <Card.Body className="contenText text-center ">
+    //     <Card.Title className="text-left ">{fecha}</Card.Title>
+    //     <Card.Title className="display-8 text-left ">{price}</Card.Title>
 
-      </Card.Body>
+    //   </Card.Body>
 
-    </Card>
+    // </Card>
   );
 };
 
--- src/components/swiper/ProductSlider.jsx
+++ src/components/swiper/ProductSlider.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import {Swiper,SwiperSlide} from 'swiper/react';
-import{FreeMode,Autoplay} from "swiper";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode, Autoplay } from "swiper";
 import 'swiper/css';
 import "swiper/css/free-mode";
-import  ProductCard  from '../landing/pure/ProductCard';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import ProductCard from '../landing/pure/ProductCard';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 // imagenes
 import img1 from '../../assets/images/image11.jpg';
@@ -15,60 +14,60 @@ import img5 from '../../assets/images/image15.jpg';
 import img6 from '../../assets/images/image16.jpg';
 import '../landing/styles/news.css'
 
-const texto="Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita sint, consequuntur quidem voluptates, ipsum maxime repellendus natus quas"
-const fecha="29 de octubre de 2003"
+const texto = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita sint, consequuntur quidem voluptates, ipsum maxime repellendus natus quas"
+const fecha = "29 de octubre de 2003"
 const ProductSlider = () => {
     return (
         <div className='conten py-5 px-4 justify-content-center'>
             <h1 className='titulo text-center  pb-5 display-5' >ULTIMAS NOTICIAS</h1>
             <Swiper
-            freeMode={true}
-            grabCursor={true}
-            modules={[FreeMode,Autoplay]}
-            className="mySwiper"
-            autoplay={true}
-            breakpoints={{
-                0:{
-                    slidesPerView:1,
-                    spaceBetween:10,
-                },
+                freeMode={true}
+                grabCursor={true}
+                modules={[FreeMode, Autoplay]}
+                className="mySwiper"
+                autoplay={true}
+                breakpoints={{
+                    0: {
+                        slidesPerView: 1,
+                        spaceBetween: 10,
+                    },
 
-                480:{
-                    slidesPerView:2,
-                    spaceBetween:10,
-                },
+                    480: {
+                        slidesPerView: 2,
+                        spaceBetween: 10,
+                    },
 
-                768:{
-                    slidesPerView:3,
-                    spaceBetween:15,
-                }
-            }}
+                    768: {
+                        slidesPerView: 3,
+                        spaceBetween: 15,
+                    }
+                }}
             >
-                
+
                 <SwiperSlide >
-                    <ProductCard data={{imgSrc:img1,price:texto,fecha:fecha}}/>
+                    <ProductCard data={{ imgSrc: img1, price: texto, fecha: fecha }} />
                 </SwiperSlide>
 
                 <SwiperSlide>
-                    <ProductCard data={{imgSrc:img2,price:texto,fecha:fecha}}/>
+                    <ProductCard data={{ imgSrc: img2, price: texto, fecha: fecha }} />
                 </SwiperSlide>
 
                 <SwiperSlide>
-                    <ProductCard data={{imgSrc:img3,price:texto,fecha:fecha}}/>
+                    <ProductCard data={{ imgSrc: img3, price: texto, fecha: fecha }} />
                 </SwiperSlide>
 
                 <SwiperSlide>
-                    <ProductCard data={{imgSrc:img4,price:texto,fecha:fecha}}/>
+                    <ProductCard data={{ imgSrc: img4, price: texto, fecha: fecha }} />
                 </SwiperSlide>
 
                 <SwiperSlide>
-                    <ProductCard data={{imgSrc:img5,price:texto,fecha:fecha}}/>
+                    <ProductCard data={{ imgSrc: img5, price: texto, fecha: fecha }} />
                 </SwiperSlide>
 
                 <SwiperSlide>
-                    <ProductCard data={{imgSrc:img6,price:texto,fecha:fecha}}/>
+                    <ProductCard data={{ imgSrc: img6, price: texto, fecha: fecha }} />
                 </SwiperSlide>
-                
+
             </Swiper>
         </div>
     );
