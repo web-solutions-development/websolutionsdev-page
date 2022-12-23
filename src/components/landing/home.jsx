528ed16845b468fa763ebedb2dc7425671ebefdd
oscar-wsd <oscartaza@websolutionsdev.com>
2022-12-23 16:31:49 -0500

    [fix] image-resize

--- src/components/landing/home.jsx
+++ src/components/landing/home.jsx
@@ -3,46 +3,34 @@ import imageComputer from '../../assets/images/landing_home_initial_computer.png
 import iconWhatsapp from '../../assets/images/whatsapp_icon.png'
 
 export const Home = () => {
-
-  return <div className="home-bg">
-    <div className="container-screen h-full">
-      <div className='home-layout'>
-        <div className='home-wsd-layout'>
-          <div className='home-title-layout'>
-            <span className='home-title-font'>
-              IMPULSA TU
-            </span>
-            <span className='home-title-font'>
-              NEGOCIO
-            </span>
+  return (
+    <div className='home-bg'>
+      <div className='container-screen h-full'>
+        <div className='flex justify-start items-center h-full'>
+          <div className='home-wsd-layout min-[1800px]:w-[800px] w-[900px] '>
+            <div className='home-title-layout'>
+              <span className='home-title-font'>IMPULSA TU</span>
+              <span className='home-title-font'>NEGOCIO</span>
+            </div>
+            <div className='home-subtitle-layout'>
+              <span className='home-subtitle-font'>Solicitando tu propia</span>
+              <span className='home-subtitle-font'>Página web</span>
+            </div>
+            <div className='home-quotation-layout'>
+              <button className='home-quotation-button'>Cotización</button>
+            </div>
+            <div className='home-contact-layout'>
+              <button className='home-contact-button'>Contáctanos</button>
+            </div>
           </div>
-          <div className='home-subtitle-layout'>
-            <span className='home-subtitle-font'>
-              Solicitando tu propia
-            </span>
-            <span className='home-subtitle-font'>
-              Página web
-            </span>
+          <div className='xl:w-full lg:w-[850px] lg:block hidden'>
+            <img src={imageComputer} className='w-full' />
           </div>
-          <div className='home-quotation-layout'>
-            <button className='home-quotation-button'>
-              Cotización
-            </button>
-          </div>
-          <div className='home-contact-layout'>
-            <button className='home-contact-button'>
-              Contáctanos
-            </button>
-          </div>
-        </div>
-        <div className='home-image-layout'>
-          <img src={imageComputer} />
         </div>
       </div>
-
+      <div className='home-icon-wss'>
+        <img src={iconWhatsapp} />
+      </div>
     </div>
-    <div className='home-icon-wss'>
-      <img src={iconWhatsapp} />
-    </div>
-  </div>
+  )
 }
