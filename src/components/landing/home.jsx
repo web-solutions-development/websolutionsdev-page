e5ecb84158b981efce1260fe3a92581e03db3cf7
sebastian-wsd <seroven@websolutionsdev.com>
2022-12-26 16:47:30 -0500

    change image in home

--- src/components/landing/home.jsx
+++ src/components/landing/home.jsx
@@ -1,52 +1,52 @@
 import './styles/home-style.css'
-import imageComputer from '../../assets/images/fondo_home.jpg'
+import imageComputer from '../../assets/images/home_img.png'
 import iconWhatsapp from '../../assets/images/whatsapp_icon.png'
 import { Typewriter } from 'react-simple-typewriter'
 
 export const Home = () => {
   return (
     <section id='home'>
-    <div className='home-bg'>
-      <div className='container-screen h-full'>
-        <div className='flex justify-start items-center h-full'>
-          <div className='home-wsd-layout min-[1800px]:w-[800px] w-[900px] z-50 '>
-            <div className='home-title-layout'>
-              <span className='home-title-font'>IMPULSA TU</span>
-              <span className='home-title-font'>NEGOCIO</span>
+      <div className='home-bg'>
+        <div className='container-screen h-full'>
+          <div className='flex justify-start items-center h-full'>
+            <div className='home-wsd-layout min-[1800px]:w-[800px] w-[900px] z-50 '>
+              <div className='home-title-layout'>
+                <span className='home-title-font'>IMPULSA TU</span>
+                <span className='home-title-font'>NEGOCIO</span>
+              </div>
+              <div className='home-subtitle-layout'>
+                <span className='home-subtitle-font'>Solicitando tu propia</span>
+                <span className='home-subtitle-font'>
+                  <Typewriter
+                    words={['Página Web', 'Aplicativo Web', 'E-commerce']}
+                    loop
+                    cursor
+                    cursorStyle='_'
+                    typeSpeed={70}
+                    deleteSpeed={50}
+                    delaySpeed={3000}
+                  />
+                </span>
+              </div>
+              <div className='home-quotation-layout'>
+                <button className='home-quotation-button'>Cotización</button>
+              </div>
+              <div className='home-contact-layout'>
+                <button className='home-contact-button'>Contáctanos</button>
+              </div>
             </div>
-            <div className='home-subtitle-layout'>
-              <span className='home-subtitle-font'>Solicitando tu propia</span>
-              <span className='home-subtitle-font'>
-                <Typewriter
-                  words={['Página Web', 'Aplicativo Web', 'E-commerce']}
-                  loop
-                  cursor
-                  cursorStyle='_'
-                  typeSpeed={70}
-                  deleteSpeed={50}
-                  delaySpeed={3000}
-                />
-              </span>
+            <div className='xl:w-full lg:w-[850px] lg:block hidden'>
+              <img src={imageComputer} className='w-full' />
             </div>
-            <div className='home-quotation-layout'>
-              <button className='home-quotation-button'>Cotización</button>
-            </div>
-            <div className='home-contact-layout'>
-              <button className='home-contact-button'>Contáctanos</button>
-            </div>
-          </div>
-          <div className='xl:w-full lg:w-[850px] lg:block hidden'>
-            <img src={imageComputer} className='w-full' />
           </div>
         </div>
-      </div>
-      <div className='home-icon-wss z-[100]'>
-        <a href='https://wa.link/7wpbkq' target="_blank" rel="noopener noreferrer">
-          <img src={iconWhatsapp} />
+        <div className='home-icon-wss z-[100]'>
+          <a href='https://wa.link/7wpbkq' target="_blank" rel="noopener noreferrer">
+            <img src={iconWhatsapp} />
 
-        </a>
+          </a>
+        </div>
       </div>
-    </div>
     </section>
   )
 }
